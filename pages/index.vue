0539ac7607b24446ae2c3e1aa32509bca50c6a64
Олег <oleg_zub00@mail.ru>
2022-04-27 23:37:20 +0300

    +- Разметка

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,44 @@
 <template>
   <div>
-    <v-container>
-      <v-form>
-        <v-row>
-          <v-col>
-            <v-text-field
-              outlined
-              label="Название"
-              placeholder="Заглушка"
-            ></v-text-field>
-            <v-btn elevation="7">Кнопка</v-btn>
-          </v-col>
-          <v-col>
-            <v-card elevation="7">
-              <div class="card-container"></div>
+    <v-container class="w-100" style="max-width: 1200px;">
 
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-form>
+      <v-row class="action-buttons">
+        <v-col class="col-auto">
+          <v-btn elevation="7">Кнопка</v-btn>
+        </v-col>
+        <v-col class="col-auto">
+          <v-btn elevation="7">Кнопка</v-btn>
+        </v-col>
+      </v-row>
+
+      <div class="contact-list d-flex flex-column">
+
+        <div class="contact-list-item">
+          <v-card elevation="7">
+            <v-row style="padding: 20px">
+              <v-col>Content</v-col>
+              <v-col class="col-auto">
+                <v-btn >Actions</v-btn>
+              </v-col>
+            </v-row>
+          </v-card>
+        </div>
+
+        <div class="contact-list-item">
+          <v-card elevation="7">
+            <v-row style="padding: 20px">
+              <v-col>Content</v-col>
+              <v-col class="col-auto">
+                <v-btn >Actions</v-btn>
+              </v-col>
+            </v-row>
+          </v-card>
+        </div>
+
+
+      </div>
+
+
     </v-container>
 
   </div>
@@ -32,4 +52,12 @@
 .card-container {
   min-height: 80px;
 }
+
+.contact-list {
+
+}
+.contact-list-item {
+  margin: 40px 0;
+}
+
 </style>
