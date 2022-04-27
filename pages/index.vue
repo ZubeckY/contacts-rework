bcf737a0c89f11fbbcb75c685a82f2d1395a14fa
main <oleg_zub00@mail.ru>
2022-04-27 12:38:35 +0300

    ui-elems

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,25 @@
 <template>
   <div>
+    <v-container>
+      <v-form>
+        <v-row>
+          <v-col>
+            <v-text-field
+              outlined
+              label="Название"
+              placeholder="Заглушка"
+            ></v-text-field>
+            <v-btn elevation="7">Кнопка</v-btn>
+          </v-col>
+          <v-col>
+            <v-card elevation="7">
+              <div class="card-container"></div>
+
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-form>
+    </v-container>
 
   </div>
 </template>
@@ -9,5 +29,7 @@
 </script>
 
 <style>
-
+.card-container {
+  min-height: 80px;
+}
 </style>
